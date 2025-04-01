fe19b7632507de0319b66bdf24d98f513800dc64
Rachid Bensouda <bensourachid@gmail.com>
2025-04-01 22:11:48 +0200

    training admin details. confirmation dialog when deleting

--- src/app/tenantapp/private/training/training-item/training-item.component.ts
+++ src/app/tenantapp/private/training/training-item/training-item.component.ts
@@ -1,42 +1,48 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter, inject } from '@angular/core';
-import { Router, RouterLink, RouterModule, provideRouter } from '@angular/router';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 import { MaterialModule } from '@src/_module/Material.Module';
 import { AppImageComponent } from '@src/app/shared/components/app-image/app-image.component';
-import { AppSignalStore } from '@src/app/_store/Signal.Store'
-
+import { AppSignalStore } from '@src/app/_store/Signal.Store';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-training-item',
   standalone: true,
   imports: [RouterLink,RouterModule,MaterialModule,CommonModule,AppImageComponent],
-  // imports: [MaterialModule,CommonModule],
   templateUrl: './training-item.component.html',
   styleUrl: './training-item.component.scss'
 })
 export class TrainingItemComponent {
-  @Input() trainingInput: any
-  @Input() api: any
+  @Input() trainingInput: any;
+  @Input() api: any;
   @Output() deleteTraining = new EventEmitter<string>();
-  trainingid:string | undefined
-  // imgid:string
+  trainingid: string | undefined;
   router = inject(Router);
-  appSignalStore = inject(AppSignalStore)
+  appSignalStore = inject(AppSignalStore);
+  dialog = inject(MatDialog);
   
   constructor() {}
 
   ngOnInit() {
-    
-    // trainingInput.imgid
-    this.trainingid=this.trainingInput.id
-    // console.log('Image id = '+this.trainingInput.get('id'))
+    this.trainingid = this.trainingInput.id;
   }
 
   onDelete(event: Event) {
     event.stopPropagation();
     if (this.trainingInput.id) {
-      console.log('trigger delete training = '+this.trainingInput.id);
-      this.appSignalStore.deleteTrainingAsync(this.trainingInput.id);
+      const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+        width: '300px'
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+          console.log('Confirmed delete training = ' + this.trainingInput.id);
+          this.appSignalStore.deleteTrainingAsync(this.trainingInput.id);
+          this.appSignalStore.setFooterMessage('Training deleted successfully', 3000);
+        }
+      });
     }
   }
 }
